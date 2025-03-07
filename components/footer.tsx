e387d128f0c531be47a64cf13fdde455792e9778
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-03-07 12:06:07 +0530

    added new styles for the social media account links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,41 +1,66 @@
 import Link from "next/link";
-/* eslint-disable react/no-unescaped-entities */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faTwitter,
+    faGoogle,
+    faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import styles from "./Footer.module.css"; // Ensure this file exists with your custom styles
 
 const FooterComponent = () => {
     return (
-        <footer className="sm:h-screen overflow-hidden py-24 bg-black">
-            <div
-                id="container"
-                className=" px-6 sm:px-24 overflow-hidden w-full h-full"
-            >
-                <div className="w-full h-full justify-center items-center flex flex-col mb-6 sm:mb-0">
-                    <p className="font-bebas leading-none text-white text-[12vw]">
+        <footer className="sm:h-screen overflow-hidden py-24 bg-black relative">
+            <div className="px-6 sm:px-24 overflow-hidden w-full h-full relative">
+                <div className="w-full h-full flex flex-col justify-center items-center mb-6 sm:mb-0">
+                    <p className="font-bebas leading-none text-white text-[12vw] mb-20">
                         Let's talk
                     </p>
                     <div className="py-4"></div>
-                    <ul className="flex justify-center sm:flex-row items-center sm:gap-6">
-                        <li className="flex gap-3 border-2 border-white hover:bg-white text-white hover:text-black duration-300 mx-3 rounded-full px-3 py-1">
+                    <ul className={styles.socialLinks}>
+                        <li>
                             <Link
-                                href="https://www.x.com/chaitu_exe/"
+                                href="https://www.facebook.com"
                                 target="_blank"
                             >
-                                Dribble
+                                <FontAwesomeIcon
+                                    icon={faFacebook}
+                                    className={styles.icon}
+                                />
+                                <span> - Facebook</span>
                             </Link>
                         </li>
-                        <li className="flex gap-3 border-2 border-white hover:bg-white text-white hover:text-black duration-300 mx-3 rounded-full px-3 py-1">
+                        <li>
                             <Link
-                                href="https://www.x.com/chaitu_exe/"
+                                href="https://www.twitter.com"
                                 target="_blank"
                             >
-                                Behance
+                                <FontAwesomeIcon
+                                    icon={faTwitter}
+                                    className={styles.icon}
+                                />
+                                <span> - Twitter</span>
                             </Link>
                         </li>
-                        <li className="flex gap-3 border-2 border-white hover:bg-white text-white hover:text-black duration-300 mx-3 rounded-full px-3 py-1">
+                        <li>
+                            <Link href="https://www.google.com" target="_blank">
+                                <FontAwesomeIcon
+                                    icon={faGoogle}
+                                    className={styles.icon}
+                                />
+                                <span> - Google</span>
+                            </Link>
+                        </li>
+                        <li>
                             <Link
-                                href="https://www.x.com/chaitu_exe/"
+                                href="https://www.instagram.com"
                                 target="_blank"
                             >
-                                Instagram
+                                <FontAwesomeIcon
+                                    icon={faInstagram}
+                                    className={styles.icon}
+                                />
+                                <span> - Instagram</span>
                             </Link>
                         </li>
                     </ul>
